7efc377a12075931532723f481b1fecbe92461a1
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2025-02-18 14:53:05 +0100

    chore: bump binaries to 0.4.0

--- Formula/meroctl.rb
+++ Formula/meroctl.rb
@@ -1,20 +1,20 @@
 class Meroctl < Formula
   desc "Command-line tool for Calimero Network"
   homepage "https://github.com/calimero-network/core"
-  version "0.3.1"
+  version "0.4.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/calimero-network/core/releases/download/meroctl-0.3.1/meroctl_aarch64-apple-darwin.tar.gz"
-    sha256 "a008b46f1dedaca96a317e6bb9ca85e4d189f59be59bbfc53772b1a0503e1972"
+    url "https://github.com/calimero-network/core/releases/download/0.4.0/meroctl_aarch64-apple-darwin.tar.gz"
+    sha256 "4ea590f6811be28cbfd35a5f0bffaf2fbc1b6639ab62f70f6356186dc823bafd"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/calimero-network/core/releases/download/meroctl-0.3.1/meroctl_x86_64-apple-darwin.tar.gz"
-    sha256 "73598bb383ad3c8874614261d968f4b242d93282a1c2512e7cb7a4698fe425db"
+    url "https://github.com/calimero-network/core/releases/download/0.4.0/meroctl_x86_64-apple-darwin.tar.gz"
+    sha256 "cd21e2df5530749e6b056345da4b9a8dc042cef645c9bca78998e4c23cf88e34"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/calimero-network/core/releases/download/meroctl-0.3.1/meroctl_aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "534d46c121da873cb4fba7e63ac93a91ac63ecd8d49ea9f3fe78e571241163e2"
+    url "https://github.com/calimero-network/core/releases/download/0.4.0/meroctl_aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "6bbc8d6a212eff01202eef157df9b6865ccae848a7998dfaced3e28d7635072f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/calimero-network/core/releases/download/meroctl-0.3.1/meroctl_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "0d78abc491aa4d72c791f25e111bd9cc663b1b10cdc03cc732e5e9cf6bcbd47a"
+    url "https://github.com/calimero-network/core/releases/download/0.4.0/meroctl_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "7b5866323d1bbb947df73ca2acf40597e0f6858e625af8c7cdf05dbf6028d136"
   else
     odie "Unsupported platform"
   end
