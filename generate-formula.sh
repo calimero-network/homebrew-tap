41651872556548f53c79d745a442f060cbf03cbb
chefsale <sandi.fatic@gmail.com>
2025-09-30 19:52:27 +0200

    Update generate-formula.sh supported binaries: merod, meroctl, mero-abi, cargo-mero

--- generate-formula.sh
+++ generate-formula.sh
@@ -19,9 +19,9 @@ calculate_sha256() {
 
 declare -A DESCRIPTIONS
 DESCRIPTIONS=(
-  ["meroctl"]="Command-line tool for Calimero Network"
   ["merod"]="Command-line tool for Calimero Network setup"
-  ["calimero-abi"]="CLI tool for extracting Calimero WASM ABI"
+  ["meroctl"]="Command-line tool for Calimero Network"
+  ["mero-abi"]="CLI tool for extracting Calimero WASM ABI"
   ["cargo-mero"]="Cargo subcommand for Calimero Network tooling"
 )
 
@@ -33,9 +33,9 @@ DESCRIPTION=${DESCRIPTIONS[$BINARY]}
 
 declare -A PRETTY_NAMES
 PRETTY_NAMES=(
-  ["meroctl"]="Meroctl"
   ["merod"]="Merod"
-  ["calimero-abi"]="CalimeroAbi"
+  ["meroctl"]="Meroctl"
+  ["mero-abi"]="MeroAbi"
   ["cargo-mero"]="CargoMero"
 )
 
